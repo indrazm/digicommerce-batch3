44e632dc5bc2ed0c773218bc1dec7c20ad1a5064
Indra Zulfi <me@indrazm.com>
2023-12-17 20:55:51 +0700

    feat: api GET POST products

---
+++ src/app/api/products/route.js
@@ -0,0 +1,90 @@
+import { NextResponse } from "next/server";
+import prisma from "@/utils/prisma";
+import { uploadFile } from "@/lib/uploadFile";
+import slugify from "slugify";
+
+export async function GET() {
+  try {
+    const allProducts = await prisma.product.findMany({
+      include: {
+        user: {
+          select: {
+            username: true,
+          },
+        },
+      },
+    });
+    return NextResponse.json({ data: allProducts, message: "All Products fetched successfully" });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error fetching products" });
+  }
+}
+
+export async function POST(request) {
+  const formData = await request.formData();
+  const name = formData.get("name");
+  const description = formData.get("description");
+  const price = formData.get("price");
+  const featuredImage = formData.get("featuredImage");
+  const images = formData.getAll("images");
+  const category = formData.get("category");
+  const userId = formData.get("userId");
+
+  let productId = "";
+
+  // Save product ke database
+  try {
+    const allImages = [];
+    images.forEach((image) => {
+      allImages.push(image.name);
+    });
+
+    const createProduct = await prisma.product.create({
+      data: {
+        name,
+        slug: slugify(name, { lower: true, replacement: "-" }),
+        description,
+        price: Number(price),
+        featuredImage: featuredImage.name,
+        images: allImages,
+        category,
+        userId,
+      },
+    });
+
+    productId = createProduct.id;
+    console.log(createProduct);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // Send Image ke AWS S3
+  try {
+    //   Upload featured image file
+    const uploadFeaturedImage = await uploadFile({
+      Body: featuredImage,
+      Key: featuredImage.name,
+      ContentType: featuredImage.type,
+      Dir: `products/${productId}`,
+    });
+    console.log(uploadFeaturedImage);
+
+    //   Upload images file
+    images.forEach(async (item) => {
+      const uploadFeaturedImage = await uploadFile({
+        Body: item,
+        Key: item.name,
+        ContentType: item.type,
+        Dir: `products/${productId}`,
+      });
+      console.log(uploadFeaturedImage);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  return NextResponse.json({
+    message: "Product created successfully",
+  });
+}
--- src/lib/uploadFile.js
+++ src/lib/uploadFile.js
@@ -13,7 +13,7 @@ export async function uploadFile({ Body, Key, ContentType, Dir }) {
   const buffer = Buffer.from(bytes);
 
   const command = new PutObjectCommand({
-    Bucket: "digicommerce-batch3",
+    Bucket: "digicommerce-devscale",
     Body: buffer,
     Key: `${Dir}/${Key}`,
     ContentType,
