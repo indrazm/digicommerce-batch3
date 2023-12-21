267d94eb241eecce4b782df990edc3bb40a93786
Indra Zulfi <me@indrazm.com>
2023-12-21 21:51:29 +0700

    fix

--- src/components/cart/atom/cartAtom.js
+++ src/components/cart/atom/cartAtom.js
@@ -1,4 +1,5 @@
 import { atomWithStorage } from "jotai/utils"; // Local Storage
 import { atom } from "jotai"; // In-Memory
 
-export const cartAtom = atomWithStorage("cartAtom", []);
+export const cartAtom = atomWithStorage("cartAtom", []); // In Memory + Local Storage // Getting data from Local Storage
+export const cartAtomInMemory = atom([]);
